14da4a2ba791950274af74f58e948cb45eb3bd27
semane12 <114406855+semane12@users.noreply.github.com>
2022-11-19 04:51:36 -0800

    Update main.h

--- main.h
+++ main.h
@@ -2,10 +2,15 @@
 #define MAIN_H
 
 int _printf(const char *format, ...);
-int print_char(va_list types, char buffer[], int flags, int width, int precision, int size);
-int print_string(va_list types, char buffer[], int flags, int width, int precision, int size);
-int print_percent(va_list types, char buffer[], int flags, int width, int precision, int size);
-int print_int(va_list types, char buffer[], int flags, int width, int precision, int size);
-int print_binary(va_list types, char buffer[], int flags, int width, int precision, int size)
+int print_char(va_list types, char buffer[], int flags,
+               int width, int precision, int size);
+int print_string(va_list types, char buffer[], int flags,
+                 int width, int precision, int size);
+int print_percent(va_list types, char buffer[], int flags,
+                  int width, int precision, int size);
+int print_int(va_list types, char buffer[], int flags,
+              int width, int precision, int size);
+int print_binary(va_list types, char buffer[], int flags,
+                 int width, int precision, int size);
 
 #endif
