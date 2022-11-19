6bb3d4e4b55110bf9a0b0403d0f81d66f960e0aa
semane12 <114406855+semane12@users.noreply.github.com>
2022-11-19 04:55:52 -0800

    Update main.h

--- main.h
+++ main.h
@@ -3,14 +3,14 @@
 
 int _printf(const char *format, ...);
 int print_char(va_list types, char buffer[], int flags,
-      int width, int precision, int size);
+int width, int precision, int size);
 int print_string(va_list types, char buffer[], int flags,
-      int width, int precision, int size);
+int width, int precision, int size);
 int print_percent(va_list types, char buffer[], int flags,
-      int width, int precision, int size);
+int width, int precision, int size);
 int print_int(va_list types, char buffer[], int flags,
-      int width, int precision, int size);
+int width, int precision, int size);
 int print_binary(va_list types, char buffer[], int flags,
-      int width, int precision, int size);
+int width, int precision, int size);
 
 #endif
