00c6cb72cd7de418e91f4e919942da94ea536197
semane12 <114406855+semane12@users.noreply.github.com>
2022-11-19 06:28:53 -0800

    Update main.h

--- main.h
+++ main.h
@@ -1,7 +1,48 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
+
+/* FLAGS */
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+/* SIZE */
+#define S_LONG 2
+#define S_SHORT 1
+
+/**
+* struct int - struct op
+* 
+* @fmt: the format
+* @fn: the function associated
+*/
+struct fmt
+{
+  char fmt;
+  int (*fn)(va_list, char[], int, int, int, int);
+}
+
+/**
+* typedef struct fmt fmt_t - struct op
+* 
+* @fmt: the format
+* @fm_t: the function associated
+*/
+typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
+int handle_print(const char *fmt, int *i, va_list list,
+char buffer[], int flags, int width, int precision, int size);
+
+/********** FUNCTIONS *********/
 int print_char(va_list types, char buffer[], int flags,
 int width, int precision, int size);
 int print_string(va_list types, char buffer[], int flags,
