72636db72b7af6ea08809872758f15e4f610f463
semane12 <semanes2012@gmail.com>
2022-11-20 03:58:52 -0800

    utils c

--- main.h
+++ main.h
@@ -28,19 +28,19 @@ struct fmt
 {
 	char fmt;
 	int (*fn)(va_list, char[], int, int, int, int);
-}
+};
 
 /**
  * typedef struct fmt fmt_t - struct op
  *
  * @fmt: the format
- * @fm_t: the function associated
+ * @fmt_t: the function associated
  */
 typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i, va_list list,
-char buffer[], int flags, int width, int precision, int size);
+	char buffer[], int flags, int width, int precision, int size);
 
 /********** FUNCTIONS *********/
 int print_char(va_list types, char buffer[], int flags,
---
+++ utils.c
@@ -0,0 +1,83 @@
+#include "main.h"
+
+/**
+ * is_printable - evaluate if the char is printable
+ * @c: char to be evaluated
+ *
+ * Return: 1 if c is printable, 0 otherwise
+ */
+int is_printable(char c)
+{
+	if (c >= 32 && c < 127)
+		return (1);
+}
+
+/**
+ * append_hexa_code - append ascii in hexagnal code to buffer
+ * @buffer: array of chars
+ * @i: index at which to start appending
+ * @ascii_code: ASCII code
+ * Return: always 3
+ */
+int append_hexa_code(char ascii-code, char buffer[], int i)
+{
+	char map_to[] = "0123456789ABCDEF";
+	/* The hexa format code always 2 digit long */
+	if (ascii_code < 0)
+		ascii_code *= -1;
+
+	buffer[i++] = '\\';
+	beffer[i++] = 'x';
+
+	buffer[i++] = map_to[ascii_code / 16];
+	buffer[i] = map_to[ascii_code % 16];
+
+	return (3);
+}
+/**
+ * is_digit - verifies if a char is a digit
+ * @c: char to be evaluated
+ *
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int is-digit(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+
+	return (0);
+}
+
+/**
+ * convert_size_number - casts a number to the specified size
+ * @num: number to be casted
+ * @size: number indicating the type to be casted
+ *
+ * Return: casted value of num
+ */
+long int convert_size_number(long int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((short)num);
+
+	return ((int)num);
+}
+
+/**
+ * convert_size_unsgnd - casts a number to the specified size
+ * @num: number to be casted
+ * @size: number indicating the type to be cated
+ *
+ * Return: casted value of num
+ */
+long int convert_size_unsgnd(unsigned long int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((unsigned short)num);
+
+	return ((unsigned int)num);
+}
