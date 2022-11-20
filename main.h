1b742cf076b67b114f97e38a7db2d8307f143b23
semane12 <semanes2012@gmail.com>
2022-11-20 02:14:38 -0800

    main h

--- main.h
+++ main.h
@@ -76,4 +76,10 @@ int print_reverse(va_list types, char buffer[],
 int print_rot13string(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
+/****************             **********/
+int get_flags(const char *format, int *i);
+int get_precision(const char *format, int *i, va_list list);
+int get_size(const char *format, int *i);
+int get_width(const char *format, int *i, va_list list);
+
 #endif
