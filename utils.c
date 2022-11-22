dd7910ac1bfa06217a74c74a57dc0051fad4e0e1
semane12 <114406855+semane12@users.noreply.github.com>
2022-11-22 03:22:56 -0800

    team project

--- utils.c
+++ utils.c
@@ -1,12 +1,8 @@
 #include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdarg.h>
-#include <unistd.h>
 
 /**
- * is_printable - evaluate if the char is printable
- * @c: char to be evaluated
+ * is_printable - Evaluates if a char is printable
+ * @c: Char to be evaluated.
  *
  * Return: 1 if c is printable, 0 otherwise
  */
@@ -14,33 +10,36 @@ int is_printable(char c)
 {
 	if (c >= 32 && c < 127)
 		return (1);
+
+	return (0);
 }
 
 /**
- * append_hexa_code - append ascii in hexagnal code to buffer
- * @buffer: array of chars
- * @i: index at which to start appending
- * @ascii_code: ASCII code
- * Return: always 3
+ * append_hexa_code - Append ascci in hexadecimal code to buffer
+ * @buffer: Array of chars.
+ * @i: Index at which to start appending.
+ * @ascii_code: ASSCI CODE.
+ * Return: Always 3
  */
 int append_hexa_code(char ascii_code, char buffer[], int i)
 {
 	char map_to[] = "0123456789ABCDEF";
-	/* The hexa format code always 2 digit long */
+	/* The hexa format code is always 2 digits long */
 	if (ascii_code < 0)
 		ascii_code *= -1;
 
 	buffer[i++] = '\\';
-	beffer[i++] = 'x';
+	buffer[i++] = 'x';
 
 	buffer[i++] = map_to[ascii_code / 16];
 	buffer[i] = map_to[ascii_code % 16];
 
 	return (3);
 }
+
 /**
- * is_digit - verifies if a char is a digit
- * @c: char to be evaluated
+ * is_digit - Verifies if a char is a digit
+ * @c: Char to be evaluated
  *
  * Return: 1 if c is a digit, 0 otherwise
  */
@@ -53,11 +52,11 @@ int is_digit(char c)
 }
 
 /**
- * convert_size_number - casts a number to the specified size
- * @num: number to be casted
- * @size: number indicating the type to be casted
+ * convert_size_number - Casts a number to the specified size
+ * @num: Number to be casted.
+ * @size: Number indicating the type to be casted.
  *
- * Return: casted value of num
+ * Return: Casted value of num
  */
 long int convert_size_number(long int num, int size)
 {
@@ -70,11 +69,11 @@ long int convert_size_number(long int num, int size)
 }
 
 /**
- * convert_size_unsgnd - casts a number to the specified size
- * @num: number to be casted
- * @size: number indicating the type to be cated
+ * convert_size_unsgnd - Casts a number to the specified size
+ * @num: Number to be casted
+ * @size: Number indicating the type to be casted
  *
- * Return: casted value of num
+ * Return: Casted value of num
  */
 long int convert_size_unsgnd(unsigned long int num, int size)
 {
