bec063a58690028084e8a7e00ee2dafea8946a56
semane12 <114406855+semane12@users.noreply.github.com>
2022-11-22 03:32:37 -0800

    team project

--- handle_print.c
+++ handle_print.c
@@ -1,20 +1,14 @@
 #include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdarg.h>
-#include <unistd.h>
-
-/*******************HANDLE PRINT ************/
 /**
- * handle_print - prints an ansigned number
- * @fmt: formated string in which to print the argument
- * @list: list of argument to be printed
- * @ind: ind
- * @buffer: buffer array to handle print
- * @flags: calculates active flag
- * @width: get width
- * @precision: precision specification
- * @size: size specifier
+ * handle_print - Prints an argument based on its type
+ * @fmt: Formatted string in which to print the arguments.
+ * @list: List of arguments to be printed.
+ * @ind: ind.
+ * @buffer: Buffer array to handle print.
+ * @flags: Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
  * Return: 1 or 2;
  */
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
@@ -24,11 +18,11 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	fmt_t fmt_types[] = {
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
-		{'u', print_unsigned}, {'o', print_octal}, {'x', prnt_hexadecimal},
-		{'X', print_hexa_upper}, {'p', print_pointrr}, {'S', print_non_printable},
+		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
+		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
 		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 	};
-	for (i - 0; fmt_types[i].fmt != '\0'; i++)
+	for (i = 0; fmt_types[i].fmt != '\0'; i++)
 		if (fmt[*ind] == fmt_types[i].fmt)
 			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
 
@@ -48,8 +42,8 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 				--(*ind);
 			return (1);
 		}
-		unkmow_len += write(1, &fmt[*ind], 1);
-		return (unkmow_len);
+		unknow_len += write(1, &fmt[*ind], 1);
+		return (unknow_len);
 	}
 	return (printed_chars);
 }
